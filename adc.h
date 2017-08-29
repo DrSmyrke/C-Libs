7d06f2f93ab2de33cadb28ca8f2d2eb047f3a674
DrSmyrke <drsmyrke@gmail.com>
2017-08-29 23:31:51 +0300

    bug fixes

--- adc.h
+++ adc.h
@@ -1,16 +1,15 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 float vcc;
-//ADC End of Conversion interrupt 
+//int16_t vcc;
 ISR(ADC_vect){
-	unsigned char adc_data;
-	adc_data = ADC>>2; //read 8 bit value 
-	vcc = 1.1 * 255 / adc_data;
-	if(!menu){
-		sprintf(buff, "%f V",vcc);lcd_goto_xy(1,1);lcd_str(buff);
-	}
+	vcc = ADCW*(5/1023);
+// 	unsigned char adc_data;
+// 	adc_data = ADC>>2;
+// 	vcc = 1.1 * 255 / adc_data;
 }
-void adc_init(){ 
-	ADMUX = 0xE; //Set the Band Gap voltage as the ADC input
-	ADCSRA = (1<<ADEN)|(1<<ADFR)|(1<<ADIE)|(1<<ADSC)|5;
+void adc_init(){
+	ADMUX = 0b01000011;
+	ADCSRA = (1<<ADEN)|(1<<ADIE)|(1<<ADSC)|5;
+	sei ();
 }
