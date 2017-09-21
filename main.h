64c74bb5844f5d06ecbbc601ef4dd811f03fde60
DrSmyrke <drsmyrke@gmail.com>
2017-09-21 22:27:11 +0300

    Bug fixes & added T0 from mega168

--- main.h
+++ main.h
@@ -18,28 +18,33 @@
 #define setZero(reg,bit) reg &= ~(1<<bit)
 #define ibi(reg,bit) reg ^= (1<<bit)
 #define CheckBit(reg,bit) (reg&(1<<bit))
-#define nop() {asm("nop");}		// функция асемблера для задержки
-void delay(unsigned int t){			// функция задержки
-	int x;
-	for (x=t;x>0;x--) nop(); 
-}
-// Функция задержки в мкс
-void delay_us(unsigned char time_us)
-{
-	register unsigned char i;
-	for(i = 0; i < time_us; i++){
-		asm volatile(" PUSH  R0 ");
-		asm volatile(" POP   R0 ");
-	}
-}
- // Функция задержки в мс
-void delay_ms(unsigned int time_ms)
-{
-	register unsigned int i;
-	for(i = 0; i < time_ms; i++){
-		delay_us(250);
-		delay_us(250);
-		delay_us(250);
-		delay_us(250);
-	}
-}
+#define nop() {asm("nop");}
+
+// CheckBit(PINB,1);
+// setPlus(PORTB,0);
+
+// void delay(unsigned int t){
+// 	int x;
+// 	for (x=t;x>0;x--) nop(); 
+// }
+// // Функция задержки в мкс
+// void delay_us(unsigned char time_us)
+// {
+// 	register unsigned char i;
+// 	for(i = 0; i < time_us; i++){
+// 		asm volatile(" PUSH  R0 ");
+// 		asm volatile(" POP   R0 ");
+// 	}
+// }
+//  // Функция задержки в мс
+// void delay_ms(unsigned int time_ms)
+// {
+// 	register unsigned int i;
+// 	for(i = 0; i < time_ms; i++){
+// 		delay_us(250);
+// 		delay_us(250);
+// 		delay_us(250);
+// 		delay_us(250);
+// 	}
+// }
+
