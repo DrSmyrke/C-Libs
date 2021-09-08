d881a86c25721b3e018ff6ac3ea877988777d6f4
Jura Prokofev <Smyrke2005@yandex.ru>
2021-09-08 17:42:55 +0300

    Add ring_buffer lib

---
+++ STM/ring_buffer.c
@@ -0,0 +1,51 @@
+/* Includes ------------------------------------------------------------------*/
+#include "ring_buffer.h"
+
+
+
+/* Variables -----------------------------------------------------------------*/
+
+
+
+
+/* Functions -----------------------------------------------------------------*/
+void RING_BUFFER_init(RingBuffer *buffer, const uint16_t size)
+{
+	uint8_t buff[ size ];
+	
+	buffer->pBuffer		= buff;
+	buffer->size		= size;
+	
+	RING_BUFFER_clear( buffer );
+}
+
+uint16_t RING_BUFFER_available(const RingBuffer *buffer)
+{
+	return ((uint16_t)(buffer->size + buffer->wIndx - buffer->rIndx)) % buffer->size;
+}
+
+uint8_t RING_BUFFER_read(RingBuffer *buffer)
+{
+	if( buffer->wIndx == buffer->rIndx ){
+		return 0;
+	}else{
+		uint8_t byte = buffer->pBuffer[ buffer->rIndx ];
+		buffer->rIndx = (uint16_t)(buffer->rIndx + 1) % buffer->size;
+	}
+}
+
+void RING_BUFFER_clear(RingBuffer *buffer)
+{
+	buffer->rIndx = 0;
+	buffer->wIndx = 0;
+
+	for( uint16_t i = 0; i < buffer->size; i++ ){
+		buffer->pBuffer[ i ] = '\0';
+	}
+}
+
+void RING_BUFFER_write(RingBuffer *buffer, const uint8_t value)
+{
+	buffer->pBuffer[ buffer->wIndx ];
+	buffer->wIndx = (uint16_t)(buffer->wIndx + 1) % buffer->size;
+}
---
+++ STM/ring_buffer.h
@@ -0,0 +1,44 @@
+#ifndef __RING_BUFFER_H__
+#define __RING_BUFFER_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+
+
+/* Defines -------------------------------------------------------------------*/
+
+
+
+
+/* Variables -----------------------------------------------------------------*/
+#pragma pack(push, 1)
+typedef struct {
+	uint8_t *pBuffer;
+	uint16_t wIndx;
+	uint16_t rIndx;
+	uint16_t size;
+} RingBuffer;
+#pragma pack(pop)
+
+
+
+/* Functions -----------------------------------------------------------------*/
+void RING_BUFFER_init(RingBuffer *buffer, const uint16_t size);
+uint16_t RING_BUFFER_available(const RingBuffer *buffer);
+uint8_t RING_BUFFER_read(RingBuffer *buffer);
+void RING_BUFFER_clear(RingBuffer *buffer);
+void RING_BUFFER_write(RingBuffer *buffer, const uint8_t value);
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __RING_BUFFER_H__ */
