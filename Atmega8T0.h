fc73a0e25dced5511285cdc8d4e065ab7ebc7059
Jura Prokofev <Smyrke2005@yandex.ru>
2022-04-06 18:33:04 +0300

    Atmega8T0 Lib

--- Atmega8T0.h
+++ Atmega8T0.h
@@ -1,24 +1,14 @@
-#include <avr/interrupt.h>
-#include <avr/io.h>
+#ifndef _ATMEGA8_T0_H_
+#define _ATMEGA8_T0_H_
 
-void second(void);
+#include <stdint.h>
 
-//Вектор при переполнении счетчика
-ISR( TIMER0_OVF_vect )
-{
-	static uint16_t counter = 0;
-	if(counter < 15625){
-		counter++;
-	}else{
-		counter = 0;
-		second();
-	}
-}
-void t0_init()
-{
-	TCCR0|=(1<<CS00);//предделитель/1
-	//TCCR0|=(1<<CS01);//предделитель/8
-	//TCCR0|=(1<<CS01)|(1<<CS00);//предделитель/64
-	//TCCR0|=(1<<CS02)|(1<<CS00);//предделитель/1024
-	TIMSK|=(1<<TOIE0);		// Разрешаем прерывание по переполнению Т0
-}
+extern uint8_t ms100Flag;
+extern uint8_t secondFlag;
+
+
+void t0_reset(void);
+void t0_init(void);
+
+
+#endif // _ATMEGA8_T0_H_
