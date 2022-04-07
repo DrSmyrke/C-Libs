4260e1c7009e1a968c537dbed91c5e0a9c910667
Jura Prokofev <Smyrke2005@yandex.ru>
2022-04-07 10:39:54 +0300

    Update functions.h

--- STM/HAL/functions.h
+++ STM/HAL/functions.h
@@ -1,9 +1,13 @@
+#ifdef __ARMCC_VERSION
+
 #ifndef __FUNCTIONS
 #define __FUNCTIONS
 
 #include <stdarg.h>
-#include <stm32f1xx_hal_uart.h>
 
 void UART_Printf(UART_HandleTypeDef * huart, const char* fmt, ...);
 
+
+
 #endif //__FUNCTIONS
+#endif //__ARMCC_VERSION
